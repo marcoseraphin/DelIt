0dcc61c25e43e0c446fb202868bac0ecbec637ab
Fayddelight <veitebbers@hotmail.de>
2021-11-09 16:37:57 +0100

    Andere User berechtigen granted

---
+++ delt/Views/GrantPermissionStateAcceptedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace delt.Views
+{
+    public partial class GrantPermissionStateAcceptedPage : ContentPage
+    {
+        public GrantPermissionStateAcceptedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
