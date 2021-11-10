a1b7286c3ec0ddc84baee651ac53eff947161d25
Fayddelight <veitebbers@hotmail.de>
2021-11-10 08:32:07 +0100

    * QRCode.svg: updated commit

---
+++ delt/Views/GrantPermissionStateLinkPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace delt.Views
+{
+    public partial class GrantPermissionStateLinkPage : ContentPage
+    {
+        public GrantPermissionStateLinkPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
