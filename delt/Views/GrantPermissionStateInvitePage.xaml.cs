a4e97302903542780c68b98cc6e4f0347f82e086
Fayddelight <veitebbers@hotmail.de>
2021-11-23 13:11:38 +0300

    new commit

---
+++ delt/Views/GrantPermissionStateInvitePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace delt.Views
+{
+    public partial class GrantPermissionStateInvitePage : ContentPage
+    {
+        public GrantPermissionStateInvitePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
